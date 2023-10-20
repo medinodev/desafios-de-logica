12623749e9251f71b1deb2de60440b96ef78f0ab
medinodev <medino.dev@gmail.com>
2023-10-20 14:54:36 -0300

    correcao de codigo

--- desafio1.js
+++ desafio1.js
@@ -1,31 +1,31 @@
-let name = 'mcthink'
-let quantDeXp = 8590
+const name = 'Mcthink'
+var quantDeXp = 8590
 
 /*Quando voce pede entre dois numeros eu nao deveria considerar o numero inicial e final como no exemplo dois, se está entre o 1.001 e 2.000 eu nao deveria considera-los. Entretanto se eu nao considerar e meu xp foi 1.001 ele nao teria um nivel.*/
 
 if(quantDeXp < 1000){
-    let nivel = 'Ferro'
+    var nivel = 'Ferro'
 }
-else if(1001 <= quantDeXp <= 2000){
-    let nivel = 'Bronze'
+else if(quantDeXp >= 1001 && quantDeXp <= 2000){
+    var nivel = 'Bronze'
 }
-else if(2001 <= quantDeXp <= 5000){
-    let nivel = 'Prata'
+else if(quantDeXp >= 2001 && quantDeXp <= 5000){
+    var nivel = 'Prata'
 }
-else if(6001 <= quantDeXp <= 7000){
-    let nivel = 'Ouro'
+else if(quantDeXp >= 6001 && quantDeXp <= 7000){
+    var nivel = 'Ouro'
 }
-else if(7001 <= quantDeXp <= 8000){
-    let nivel = 'Platina'
+else if(quantDeXp >= 7001 && quantDeXp <= 8000){
+    var nivel = 'Platina'
 }
-else if(8001 <= quantDeXp <= 9000){
-    let nivel = 'Ascendente'
+else if(quantDeXp >= 8001 && quantDeXp <= 9000){
+    var nivel = 'Ascendente'
 }
-else if(9001 <= quantDeXp <= 10000){
-    let nivel = 'Imortal'
+else if(quantDeXp >= 9001 && quantDeXp <= 10000){
+    var nivel = 'Imortal'
 }
 else if(quantDeXp >= 10001){
-    let nivel = 'Radiante'
+    var nivel = 'Radiante'
 }
 
-console.log(`O Herói de nome ${nome} está no nível de ${nivel}.`)
+console.log('O Herói de nome ' + name + ' está no nível de ' + nivel)
