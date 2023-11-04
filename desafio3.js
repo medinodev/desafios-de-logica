2c0c4e22152a92100990a7d3668d1605ccd43ddd
medinodev <medino.dev@gmail.com>
2023-11-04 20:18:53 -0300

    new code

--- desafio3.js
+++ desafio3.js
@@ -1,18 +1,36 @@
+const posicaoInicial = 20
+const totalPassos = 6
+
+//TODO: Calcule a posição final do herói:/
+//const posicaoFinal = calcPosition(posicaoFinal, totalPassos);
+
+function calcPosition(x, y){
+  var soma = x + y;
+  return soma;
+}
+
+var z = calcPosition(posicaoInicial, totalPassos);
+
+// Imprime a posição final
+console.log("Posicao final do heroi:" + z);
+
+//CODIGO COM COMENTARIOS DA PLATAFORMA
+
 //Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
 //- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
 //- "print": imprime um texto de saída (output), pulando linha.
 
 // Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
-const posicaoInicial = parseInt(gets());
-const totalPassos = parseInt(gets());
+const posiInicial = parseInt(gets());
+const tPassos = parseInt(gets());
 
 //TODO: Calcule a posição final do herói:
-const posicaoFinal = calcPosition(posicaoInicial, totalPassos);
+const posiFinal = calcPosition(posiInicial, tPassos);
 
 function calcPosition(x, y){
-  var soma = x + y;
-  return soma
+  var soman = x + y;
+  return soman
 }
 
 // Imprime a posição final
-console.log("Posicao final do heroi: " + posicaoFinal);
+console.log("Posicao final do heroi: " + posiFinal);
